245db35ba597f5a89ecd61791e374c81f2f2f4f8
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-11-14 20:32:42 -0300

    primeiro commit

---
+++ models/model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const readingSchema = new mongoose.Schema(
+  {
+    humidity: {
+      type: Number,
+      required: true,
+    },
+    temperature: {
+      type: Number,
+      required: true,
+    },
+    mq_sensor_value: {
+      type: Number,
+      required: true,
+    },
+    mq_voltage: {
+      type: Number,
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      default: Date.now, // Define um valor padrão usando a data e hora atuais
+    },
+  },
+  { timestamps: true },
+); 
+
+const Reading = mongoose.model('Reading', readingSchema);
+
+module.exports = Reading;
