05326c32f521e17aa02287de12a5ab2c8c927555
cristianfatec <cristian.nascimento@fatec.sp.gov.br>
2024-11-16 17:38:49 -0300

    5ª atualização para deploy no Vercel (users.js 2)

--- api/users.js
+++ api/users.js
@@ -5,15 +5,18 @@ export default async function handler(req, res) {
   // Conectando ao banco
   await connectToDatabase();
 
+  // Determina a subrota (caminho após /api/users)
+  const subRoute = req.url;
+
   if (req.method === 'POST') {
-    if (req.url === '/users/register') {
+    if (subRoute === '/register') {
       return register(req, res);
-    } else if (req.url === '/users/login') {
+    } else if (subRoute === '/login') {
       return login(req, res);
     } else {
       return res.status(404).json({ message: 'Endpoint não encontrado!' });
     }
   } else {
-    return res.status(405).json({ message: 'Método não permitido!' });
+    return res.status(405).json({ message: 'Method Not Allowed' });
   }
 }
